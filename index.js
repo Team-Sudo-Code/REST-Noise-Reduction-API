f16166f5420ec808b3c797b623a931d406995733
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2019-06-23 15:57:23 -0700

    Potential port fix

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', function (req, res) {
     res.end("Get request worked!");
 })
 
-var server = app.listen(8081, function () {
+var server = app.listen(process.env.port || 8000, function () {
    var host = server.address().address;
    var port = server.address().port;
    console.log("Example app listening at http://%s:%s", host, port);
