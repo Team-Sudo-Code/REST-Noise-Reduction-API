56ee0a7a68ad24d16f038aa0443fe602410865cf
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2019-06-23 23:41:32 -0700

    Added request playground

--- index.js
+++ index.js
@@ -3,20 +3,30 @@ var fs = require('fs');
 var request = require('request');
 var express = require('express');
 var favicon = require('serve-favicon');
+var bodyParser = require('body-parser');
 var app = express();
 
+app.use(bodyParser.json());
+
 app.get('/get', function (req, res) {
-    res.write("Your GET request was successful!\nData received:\n"+JSON.stringify(req.query||{}));
+    res.json({
+        status: "Your GET request was successful!\nData received:\n" +
+            JSON.stringify(req.query || {})
+    });
     res.end();
 });
 
 app.post('/post', function (req, res) {
-    res.write("Your POST request was successful!\nData received:\n"+JSON.stringify(req.query||{}));
+    res.json({
+        status: "Your POST request was successful!\nData received:\n" +
+            JSON.stringify(req.body || {})
+    });
     res.end();
 });
 
 app.get('/', function (req, res) {
-    res.redirect("https://github.com/Team-Sudo-Code/Video-Noise-Reduction-API");
+    //    res.redirect("https://github.com/Team-Sudo-Code/Video-Noise-Reduction-API");
+    res.sendFile(__dirname + "/index.html");
 });
 
 app.use(favicon(__dirname + '/favicon.ico'));
