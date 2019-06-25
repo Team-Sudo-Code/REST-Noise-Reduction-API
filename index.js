fb626712c09030a7df5f735af1138c723cfedf88
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2019-06-24 21:32:29 -0700

    Switched to dark mode!

--- index.js
+++ index.js
@@ -32,5 +32,5 @@ app.get('/', function (req, res) {
 app.use(favicon(__dirname + '/favicon.ico'));
 
 app.listen(process.env.PORT || 8000, () =>
-    console.log(`Example app listening on port ${process.env.PORT || 8000}!`),
+    console.log(`Server listening on port ${process.env.PORT || 8000}!`),
 );
