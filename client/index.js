fdb8a564d9ee68169886bbc04ae80860598ece53
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2019-06-28 23:58:41 -0700

    Added client index page

---
+++ client/index.js
@@ -0,0 +1,17 @@
+window.onload = ()=>{
+    /*
+    var results = ffmpeg_run({
+        arguments: [string],
+        files: [
+            {
+                data: UInt8Array,
+                name: string
+            }
+        ]
+    });
+
+    results.forEach(function (file) {
+        console.log("File recieved", file.name, file.data);
+    });
+    */
+}
